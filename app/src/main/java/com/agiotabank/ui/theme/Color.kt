00abbf711dd943d37c4acb7626914def643e32ee
Eduardo Fabri <161086246+eduardofabrii@users.noreply.github.com>
2025-10-10 23:43:31 -0300

    feat: add estrutura inicial da home

--- app/src/main/java/com/agiotabank/ui/theme/Color.kt
+++ app/src/main/java/com/agiotabank/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.agiotabank.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DarkBackground = Color(0xFF0A1628)
+val CardBackground = Color(0xFF132238)
+val AccentBlue = Color(0xFF2E5C8A)
+val LightBlue = Color(0xFF4A90E2)
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xFFB0C4DE)
+val Green = Color(0xFF4CAF50)
+val Red = Color(0xFFFF453A)
